b01e3f649274bca9adc95f23b331cab7ee0adc5f
Filipe Silva <filipe.silva@opah.com.br>
2025-11-30 22:19:09 -0300

    fix: correções de metricas e docker-compose

--- src/main/java/com/pixservice/PixWalletServiceApplication.java
+++ src/main/java/com/pixservice/PixWalletServiceApplication.java
@@ -2,8 +2,10 @@ package com.pixservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class PixWalletServiceApplication {
 
 	public static void main(String[] args) {
